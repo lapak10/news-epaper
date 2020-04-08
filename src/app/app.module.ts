d6629e1516d870734654bd6fe258e8acc94f949f
anand <anandkmk@gmail.com>
2020-04-08 23:35:08 +0530

    added ngBootsrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,7 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   declarations: [
     AppComponent
   ],
-  imports: [
+  imports: [NgbModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
