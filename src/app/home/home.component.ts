3559255c07d8b2b3199f6217131931ba0d5a7368
anand <anandkmk@gmail.com>
2020-04-09 02:02:31 +0530

    added default service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { DailynewsService } from '../services/dailynews.service';
 
+// export interface DayObject {
+//   date: string,
+//   data: Array<NewsDataObject>
+// }
+// export interface NewsDataObject {
+//   thumbnail_url: String
+//   publisher: String
+// }
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [DatePipe]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ns: DailynewsService) { }
+
+  date: Date = new Date()
+
+  newspaper: any
 
   ngOnInit(): void {
+    this.newspaper = this.ns.fetch_daily_news()
+  }
+
+  view_paper(pdf: string) {
+    window.open(pdf, '_blank');
   }
 
 }
